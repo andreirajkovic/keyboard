e54028583b228608876042450e42426a447790d6
andreirajkovic <34891295+andreirajkovic@users.noreply.github.com>
2019-02-03 19:48:12 -0800

    Update __init__.py

--- keyboard/__init__.py
+++ keyboard/__init__.py
@@ -247,6 +247,7 @@ class _KeyboardListener(_GenericListener):
             if event_type == KEY_DOWN:
                 if is_modifier(scan_code): self.active_modifiers.add(scan_code)
                 _pressed_events[scan_code] = event
+                screenshot("%s.png" % _time.time(), region=CG.CGRectMake(425, 47, 547, 326))
             hotkey = tuple(sorted(_pressed_events))
             if event_type == KEY_UP:
                 self.active_modifiers.discard(scan_code)
@@ -284,7 +285,6 @@ class _KeyboardListener(_GenericListener):
                 self.modifier_states[key] = new_state
 
         if accept:
-            screenshot("%s.png" % _time.time(), region=CG.CGRectMake(425, 47, 547, 326))
             if event_type == KEY_DOWN:
                 _logically_pressed_keys[scan_code] = event
             elif event_type == KEY_UP and scan_code in _logically_pressed_keys:
